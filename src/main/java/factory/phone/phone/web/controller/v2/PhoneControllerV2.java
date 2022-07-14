e533b2ef6e546f9b8e3a54750f4f6fe24c0ff72a
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-14 11:34:40 +0300

    Added project Lombok for annotation processing.

--- src/main/java/factory/phone/phone/web/controller/v2/PhoneControllerV2.java
+++ src/main/java/factory/phone/phone/web/controller/v2/PhoneControllerV2.java
@@ -2,26 +2,25 @@ package factory.phone.phone.web.controller.v2;
 
 import factory.phone.phone.web.model.v2.PhoneDtoV2;
 import factory.phone.phone.web.service.v2.PhoneServiceV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
-@Validated
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/phone")
 @RestController
 public class PhoneControllerV2 {
 
     private final PhoneServiceV2 phoneServiceV2;
-
-    public PhoneControllerV2(PhoneServiceV2 phoneServiceV2) {
-        this.phoneServiceV2 = phoneServiceV2;
-    }
 
     @GetMapping({"/{phoneId}"})
     public ResponseEntity<PhoneDtoV2> getPhone(@NotNull @PathVariable("phoneId") UUID phoneId) {
@@ -30,9 +29,11 @@ public class PhoneControllerV2 {
 
     @PostMapping // create new phone
     public ResponseEntity handlePost(@Valid @NotNull @RequestBody PhoneDtoV2 phoneDto) {
-        PhoneDtoV2 savedDto = phoneServiceV2.saveNewPhone(phoneDto);
+        log.debug("in handle post...");
 
-        HttpHeaders headers = new HttpHeaders();
+        val savedDto = phoneServiceV2.saveNewPhone(phoneDto);
+
+        var headers = new HttpHeaders();
         headers.add("Location", "/api/v2/phone/" + savedDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
